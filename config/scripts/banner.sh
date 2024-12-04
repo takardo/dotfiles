25ad54596c0c7e933010d71928c9ae9a19c2cfc0
takardo <4397anon@gmail.com>
2024-12-04 00:07:01 -0500

    update

--- config/scripts/banner.sh
+++
@@ -1,3 +0,0 @@
-clear; figlet -f "Big Money-sw" -c "nollem64" | lolcat -a -d 1 && 
-figlet -f Bloody -c "GNU LINUX" | lolcat -a -d 1 &&
-figlet -f "Digital" -c "<EMBRACE_THE_TERMINAL/>" | lolcat -a -d 1 
