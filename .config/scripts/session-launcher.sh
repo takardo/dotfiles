666e3d85d1ca8bf870233ab8826170058e038e9b
Takardo <4397anon@gamil.com>
2025-06-27 12:45:47 -0400

    dotfiles

--- .config/scripts/session-launcher.sh
+++ .config/scripts/session-launcher.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 BLUE='\033[1;34m'
+PINK='\033[1;35m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
 RESET='\033[0m'
 
-session_names=("Hyprland" "Sway" "Plasma")
+session_names=("Hyprland" "Sway" "Plasma-Wayland")
 session_cmds=(
   "Hyprland -c /home/tumbleweed/dotfiles/.config/hypr/hyprland.conf"
   "sway -c /home/tumbleweed/dotfiles/.config/sway/config --unsupported-gpu"
@@ -27,8 +30,20 @@ done
 echo -e "${RESET}"
 
 for i in "${!session_names[@]}"; do
-  center_text "$((i + 1))) ${session_names[$i]}"
+  text="$((i + 1))) ${session_names[$i]}"
+  case $i in
+    0) # Hyprland - pink
+       printf "%*s${PINK}%s${RESET}\n" $(((term_width - ${#text}) / 2)) "" "$text"
+       ;;
+    1) # Sway - green
+       printf "%*s${GREEN}%s${RESET}\n" $(((term_width - ${#text}) / 2)) "" "$text"
+       ;;
+    2) # Plasma - orange
+       printf "%*s${ORANGE}%s${RESET}\n" $(((term_width - ${#text}) / 2)) "" "$text"
+       ;;
+  esac
 done
+
 center_text "0) Exit"
 echo
 
@@ -43,7 +58,11 @@ if [[ "$choice" == "0" ]]; then
   exit 0
 elif [[ "$choice" =~ ^[1-9][0-9]*$ && "$choice" -le "${#session_names[@]}" ]]; then
   index=$((choice - 1))
-  center_text "Launching ${session_names[$index]}..."
+  case $index in
+    0) center_text "${PINK}Launching ${session_names[$index]}...${RESET}" ;;
+    1) center_text "${GREEN}Launching ${session_names[$index]}...${RESET}" ;;
+    2) center_text "${ORANGE}Launching ${session_names[$index]}...${RESET}" ;;
+  esac
   exec ${session_cmds[$index]}
 else
   center_text "Invalid choice."
